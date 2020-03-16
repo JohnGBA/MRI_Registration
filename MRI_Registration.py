1a6baff0c87531611b479ccc8b8aa55af1773e47
JohnGBA <johnathangomez@gmail.com>
2020-03-16 18:57:00 -0300

    removed extra lines and impoved comments

--- MRI_Registration.py
+++ MRI_Registration.py
@@ -3,28 +3,16 @@ import os
 import nibabel as nib
 import cv2
 import matplotlib.pyplot as plt
-import pickle
 import random
-import scipy.misc
-from scipy import *
-from sklearn.neighbors import NearestNeighbors
-from sklearn.neighbors import KNeighborsRegressor
 from sklearn.externals import joblib
-import re
 import time
 from sklearn.ensemble import RandomForestRegressor
-from sklearn.datasets import make_regression
-import sys
-
-
 
 def delete_NaN():
-  #img_=nib.load('C:/Projet_2A/bdd'+'/allenBrainAtlas.nii')
-  #/media/johnathan/Windows/Users/abcd/Desktop/2020/codes for Github/Python ML
-  img_=nib.load('/home/johnathan/Desktop/2020/Python ML/bdd'+'/CTRL_alc_M1_t2_1.nii')
+  img_=nib.load(os.getcwd() + 'bdd/CTRL_alc_M1_t2_1.nii')
   img= img_.get_fdata()
   copy=img
-  C=0
+  C=0 #number of NaN values in image.
   
   for i in range(165):
     for j in range(230):
@@ -32,12 +20,12 @@ def delete_NaN():
         if np.isnan(img[i,j,k]) :
           C=C+1
           copy[i,j,k]=0
-  #joblib.dump(copy,'C:/Projet_2A/bdd'+'/atlas_sans_NaN.saved')
-  joblib.dump(copy,'/home/johnathan/Desktop/2020/Python ML/bdd'+'/CTRL_alc_M1_t2_1_sans_NaN.saved')
+          
+  joblib.dump(copy, os.getcwd() + 'bdd/CTRL_alc_M1_t2_1_sans_NaN.saved')
   return(C)
 
 def rotate(image, angle):
-  image_center = tuple(np.array(image.shape[1::-1]) / 2) #largeur puis hauteur
+  image_center = tuple(np.array(image.shape[1::-1]) / 2) #width then height.
   rot_mat = cv2.getRotationMatrix2D(image_center, angle, 1.0)
   result = cv2.warpAffine(image, rot_mat, image.shape[1::-1], flags=cv2.INTER_LINEAR)
   return result
@@ -45,10 +33,10 @@ def rotate(image, angle):
 def translation(image,tx,ty):
   rows,cols= image.shape
   M = np.float32([[1,0,tx],[0,1,ty]])
-  translatee = cv2.warpAffine(image,M,(cols,rows))
-  return translatee
+  translated = cv2.warpAffine(image,M,(cols,rows))
+  return translated
 
-def getvalues(image): ##recupere les donnees contenues dans la colonne et la ligne du milieu de l image
+def getvalues(image): ##retrieve the data contained in the middle column and row of the image.
   rows,cols= image.shape
   T=np.zeros((1,rows+cols))
   for i in range(rows):
@@ -57,13 +45,12 @@ def getvalues(image): ##recupere les donnees contenues dans la colonne et la lig
     T[0,rows+j]=image[rows//2,j]
   return T
 
-def add_noise2(vect1D,mag,size):
+def add_noise(vect1D,mag,size):
   vect1D+=np.random.uniform(-1*mag, 1*mag,size)
   return(vect1D)
 
 def create_bdd(path_img,path_bdd,txmin,txmax,Ntx,tymin,tymax,Nty,thetamin,thetamax,Ntheta):
-
-  ## definition des pas de translations et rotation
+  ## definition of translation and rotation step
   if Ntx!=0:
     pas_tx=float(txmax-txmin)/float(Ntx)
   else:
@@ -77,12 +64,11 @@ def create_bdd(path_img,path_bdd,txmin,txmax,Ntx,tymin,tymax,Nty,thetamin,thetam
   else:
     pas_theta=0   
   
-  ##chargement de l image nii depuis un fichier pickled
-  
+  ##loads a '.nii' image
   img_=joblib.load(path_img)
   img=img_.T[120]
 
-  ## creation de set ( la matrice qui contiendra les 2 lignes de pixels ) et labels ( matrice qui contient les translations et rotations associes
+  ## creation of dataset ( matrice containing the 2 extracted lines of pixels ) and labels ( associated translations and rotations).
   set_size=(Ntx+1)*(Nty+1)*(Ntheta+1)
   print('setsize='+str(set_size))
   rows,cols=img.shape
@@ -90,48 +76,36 @@ def create_bdd(path_img,path_bdd,txmin,txmax,Ntx,tymin,tymax,Nty,thetamin,thetam
   labels=np.zeros((set_size,3))
 
   compteur=0
-
-  ##On va maintenant creer une combinaison de translations et rotation pour creer la BDD
+  ##Combinations of all rotation and translation to form the database.
   for X in range(0,Ntx+1):
     for Y in range(0,Nty+1):
       for O in range(0,Ntheta+1):
 
-        copy=img #on cree une copie pour ne pas modifier img de base
+        copy=img #copies the input image to avoid modifying it.
 
-        ##on applique la transformation et on enregistre uniquement la colonne et ligne du milieu de l img transformee
-
-      
-        temporaire=translation(rotate(copy,thetamin+O*pas_theta),txmin+X*pas_tx,tymin+Y*pas_ty) #contient l image transformee
+        temporaire=translation(rotate(copy,thetamin+O*pas_theta),txmin+X*pas_tx,tymin+Y*pas_ty) #returns transformed image.
+        set[compteur,:]=getvalues(temporaire) #the middle column and row of the transformed image is fed to the dataset.
         
-        set[compteur,:]=getvalues(temporaire) #on nourrit la matrice d apprentissage ..
-        
-        #.. ainsi que la matrice de label
+        #Label matrix is populated.
         labels[compteur,0]=txmin+X*pas_tx
         labels[compteur,1]=tymin+Y*pas_ty
         labels[compteur,2]=thetamin+O*pas_theta
         
         compteur+=1
 
-  #on enregistre les deux matrices sous format pickle
+  #The two matrices are stored in disk
   joblib.dump(set,path_bdd+'/bdd606060CRTL.saved')
   joblib.dump(labels,path_bdd+'/labels606060CRTL.saved')
   
-#exemple pour appeler la fonction :
-#create_bdd('C:/Projet_2A/Projet_2A_random_forest/bdd/CTRL_alc_M1_t2_1_onto_allen_sans_NaN.saved','C:/Projet_2A/Projet_2A_random_forest/bdd/',-30,30,60,0,0,1,0,0,1)
-#create_bdd('/home/johnathan/Desktop/2020/Python ML/bdd/CTRL_alc_M1_t2_1_sans_NaN.saved','/home/johnathan/Desktop/2020/Python ML/Projet_2A_random_forest/bdd',-30,30,60,-30,30,60,-30,30,60)
-  
+#Example on how to call the function: 
+#create_bdd(os.getcwd() + '/bdd/CTRL_alc_M1_t2_1_sans_NaN.saved',os.getcwd() + '/bdd',-30,30,60,-30,30,60,-30,30,60)
 
-def get_fit(path_bdd,path_labels): #construit le modele a partir des data et labels
+def get_fit(path_bdd,path_labels): #Generates the model from the the data and labels
 
-
-  #on importe les donnees precedemment creees
-  
   bdd=joblib.load(path_bdd)
   labels=joblib.load(path_labels)
 
-  #on construit le modele a partir des data
-
-  ### ### ### PARTIE A CHANGER EN CAS DE CHANGEMENT DE METHODE DE REGRESSION, ICI REGRESSION RANDOM FOREST. POUR D'AUTRES METHODES ALLER SUR SK-LEARN
+  ### ### ### PART TO CHANGE IN THE EVENT OF A CHANGE IN REGRESSION METHOD. NOW RANDOM FOREST IS USED. FOR OTHER METHODS, REFER TO SK-LEARN
   t1=time.time()
   regr = RandomForestRegressor(n_estimators=20, random_state=0)
   regr.fit(bdd,labels)
@@ -139,69 +113,50 @@ def get_fit(path_bdd,path_labels): #construit le modele a partir des data et lab
   
   print('R^2=')
   print(regr.score(bdd,labels))
-
   ### ### ###
 
-  #on enregistre le modele avec pickle
-  #joblib.dump(regr,'C:/Projet_2A/Projet_2A_random_forest/bdd/fit_12.saved')
-  joblib.dump(regr, '/home/johnathan/Desktop/2020/Python ML' + '/Projet_2A_random_forest/bdd/fit_CRTL.saved')
+  #The model is stored in disk.
+  joblib.dump(regr, os.getcwd() + '/random_forest/bdd/fit_CRTL.saved')
   print(t2-t1)
 
+#Example on how to call the function: 
+#get_fit(os.getcwd() + '/random_forest/bdd/bdd606060.saved', os.getcwd() + '/random_forest/bdd/labels606060.saved')
 
-#exemple pour appeler la fonction :
-#get_fit('C:/Projet_2A/Projet_2A_random_forest/bdd/bdd.saved','C:/Projet_2A/Projet_2A_random_forest/bdd/labels.saved')
-#get_fit('/home/johnathan/Desktop/2020/Python ML/Projet_2A_random_forest/bdd/bdd3030120.saved','/home/johnathan/Desktop/2020/Python ML/Projet_2A_random_forest/bdd/labels3030120.saved')
-
-
-
-def predict(path_regr,path_img_test,N_tests): #realise des predictions a partir du modele fourni
+def predict(path_regr,path_img_test,N_tests): #Perfoms the predictions based on the provided model.
 
   x,y,theta=0,0,0
-  V=0
+  V=0 #number of good predictions.
 
   temps_moy=0
   erreur_moy_x=0
   erreur_moy_y=0
   erreur_moy_theta=0
 
-
-  
-
-  
-  #on importe une image qui sera transformee aleatoirement pour realiser des tests
+  #An image that will be randomnly transformed is loaded.
   img_=joblib.load(path_img_test)
   img=img_.T[120]
 
-  #on importe le modele
+  #The model is loaded.
   regr=joblib.load(path_regr)
   
-  res=np.zeros((N_tests,3)) #contiendra le resultat de la prediction
+  prediction=np.zeros((N_tests,3)) 
   
-
   for i in range(N_tests):
 
     copy=img
 
-    
-    
-    x=random.uniform(-30,30) #on choisit un coefficient de translation aleatoire
+    x=random.uniform(-30,30) 
     y=random.uniform(-30,30)
     theta=random.uniform(-30,30)
     
-    
-    
-    
-    #on realise une prediction et on enregistre le resultat dans res
+    #A prediction of the transformation is made and stored
     t1=time.time()
-    res[i,:]=regr.predict(getvalues(translation(rotate(copy,theta),x,y)))
+    prediction[i,:]=regr.predict(getvalues(translation(rotate(copy,theta),x,y)))
     t2=time.time()
     
-    
-    deltax=abs(x-res[i,0])
-    deltay=abs(y-res[i,1])
-    deltatheta=abs(theta-res[i,2])
-
-   
+    deltax=abs(x-prediction[i,0])
+    deltay=abs(y-prediction[i,1])
+    deltatheta=abs(theta-prediction[i,2])
 
     erreur_moy_x+=deltax
     erreur_moy_y+=deltay
@@ -210,8 +165,6 @@ def predict(path_regr,path_img_test,N_tests): #realise des predictions a partir 
       
     if (deltatheta)<1 and np.absolute(deltax + deltay*1j)<np.sqrt(2):
       V+=1
-    
-
     
     #print(deltax)
     #print(deltay)
@@ -222,62 +175,45 @@ def predict(path_regr,path_img_test,N_tests): #realise des predictions a partir 
   temps_moy=temps_moy/N_tests
   print(V,N_tests,float(V)/float(N_tests),'erreurs en x, y et theta '+str(erreur_moy_x)+', '+str(erreur_moy_y)+', '+str(erreur_moy_theta),'temps moyen de prediction '+str(temps_moy))
   
-
-  
-
-#predict('C:/Projet_2A/Projet_2A_random_forest/bdd/fit.saved','C:/Projet_2A/Projet_2A_random_forest/bdd/CTRL_alc_M1_t2_1_onto_allen_sans_NaN.saved',1)
-#predict('/home/johnathan/Desktop/2020/Python ML/Projet_2A_random_forest/bdd/fit_12.saved','/home/johnathan/Desktop/2020/Python ML/Projet_2A_random_forest/bdd/CTRL_alc_M1_t2_1.nii_sans_NaN.saved',1)    
-
+#Example on how to call the function: 
+#predict(os.getcwd() + 'random_forest/bdd/fit.saved','os.getcwd() + '/random_forest/bdd/CTRL_alc_M1_t2_1.nii_sans_NaN.saved',1)    
 
 def noisy_predict(path_regr,path_img_test,N_tests,mag): #realise des predictions a partir du modele fourni en ajoutant du bruit uniforme d'amplitude mag
 
   x,y,theta=0,0,0
-  V=0
+  V=0 #number of good predictions.
   
   temps_moy=0
   erreur_moy_x=0
   erreur_moy_y=0
   erreur_moy_theta=0
 
-
-  
-
-  
-  #on importe une image qui sera transformee aleatoirement pour realiser des tests
+  #An image that will be randomnly transformed is loaded.
   img_=joblib.load(path_img_test)
   img=img_.T[120]
   rows,cols=img.shape
   
-  #on importe le modele
+  #The model is loaded.
   regr=joblib.load(path_regr)
   
-  res=np.zeros((N_tests,3)) #contiendra le resultat de la prediction
+  prediction=np.zeros((N_tests,3)) 
   
-
   for i in range(N_tests):
 
     copy=img
 
-    
-    
-    x=random.uniform(-30,30) #on choisit un coefficient de translation aleatoire
+    x=random.uniform(-30,30) 
     y=random.uniform(-30,30)
     theta=random.uniform(-30,30)
     
-    
-    
-    
-    #on realise une prediction et on enregistre le resultat dans res
+    #A prediction is made and stored. The addnoise function is used.
     t1=time.time()
-    res[i,:]=regr.predict(add_noise2(getvalues(translation(rotate(copy,theta),x,y)),mag,rows+cols))
+    prediction[i,:]=regr.predict(add_noise(getvalues(translation(rotate(copy,theta),x,y)),mag,rows+cols))
     t2=time.time()
-    
-    
-    deltax=abs(x-res[i,0])
-    deltay=abs(y-res[i,1])
-    deltatheta=abs(theta-res[i,2])
 
-   
+    deltax=abs(x-prediction[i,0])
+    deltay=abs(y-prediction[i,1])
+    deltatheta=abs(theta-prediction[i,2])
 
     erreur_moy_x+=deltax
     erreur_moy_y+=deltay
@@ -286,8 +222,6 @@ def noisy_predict(path_regr,path_img_test,N_tests,mag): #realise des predictions
       
     if (deltatheta)<1 and np.absolute(deltax + deltay*1j)<np.sqrt(2):
       V+=1
-    
-    
     
     #print(deltax)
     #print(deltay)
@@ -298,18 +232,17 @@ def noisy_predict(path_regr,path_img_test,N_tests,mag): #realise des predictions
   temps_moy=temps_moy/N_tests
   return([float(V)/float(N_tests),erreur_moy_x,erreur_moy_y,erreur_moy_theta])
   print(V,N_tests,float(V)/float(N_tests),'erreurs en x, y et theta '+str(erreur_moy_x)+', '+str(erreur_moy_y)+', '+str(erreur_moy_theta),'temps moyen de prediction '+str(temps_moy))
-  
 
-def print_courbes(): #print des courbes qui affiche erreur et veracite en fonction de l'amplitude du bruit ajoute
-  X=zeros((1,21))
-  Y=zeros((1,21))
-  T=zeros((1,21))
-  V=zeros((1,21))
-  A=zeros((1,21))
+def print_courbes(): #print curves which displays error and accuracy as a function of the amplitude of the noise added
+  X=np.zeros((1,21))
+  Y=np.zeros((1,21))
+  T=np.zeros((1,21))
+  V=np.zeros((1,21))  #Right predictions
+  A=np.zeros((1,21))  #Noise 
   for k in range(0,1050,50):
     #with suppress_stdout():
       #res=noisy_predict('C:/Projet_2A/Projet_2A_random_forest/bdd/fit_50.saved','C:/Projet_2A/Projet_2A_random_forest/bdd/CTRL_alc_M1_t2_1_onto_allen_sans_NaN.saved',1000,k)
-      res=noisy_predict('/home/johnathan/Desktop/2020/Python ML' + '/Projet_2A_random_forest/bdd/fit_CRTL.saved', os.getcwd() + '/bdd/CTRL_alc_M1_t2_1_sans_NaN.saved',1000,k)
+      res=noisy_predict(os.getcwd() + '/random_forest/bdd/fit_CRTL.saved', os.getcwd() + '/bdd/CTRL_alc_M1_t2_1_sans_NaN.saved',1000,k)
       V[0,k//50]=res[0]
       X[0,k//50]=res[1]
       Y[0,k//50]=res[2]
@@ -318,18 +251,18 @@ def print_courbes(): #print des courbes qui affiche erreur et veracite en foncti
 
   plt.figure(1)
   plt.plot(A[0,:],V[0,:], 'g')
-  plt.xlabel('Amplitude du bruit')
-  plt.ylabel('Taux veracite (pour 1000 tests)')
-  plt.title('Taux veracite en fonction du bruit')
+  plt.xlabel('Amplitude of noise')
+  plt.ylabel('Sucess rate (for a 1000 tests)')
+  plt.title('Sucess rate in function of noise')
   plt.show()
 
   plt.figure(2)
   plt.plot(A[0,:],X[0,:], 'g')
   plt.plot(A[0,:],Y[0,:], 'r')
   plt.plot(A[0,:],T[0,:], 'b')
-  plt.xlabel('Amplitude du bruit')
-  plt.ylabel('Erreur en x(vert), y(rouge) et theta(bleu) (pour 1000 tests)')
-  plt.title('Erreurs en fonction du bruit')
+  plt.xlabel('Amplitude of noise')
+  plt.ylabel('Error in x(green), y(red) and theta(blue) (for a 1000 tests)')
+  plt.title('Errors in function of noise')
   plt.show()
     
 
